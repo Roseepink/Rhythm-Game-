a0e2363b304aadbfef9928f3c4527075c561de4f
Unknown <SK2060@live.mdx.ac.uk>
2019-10-30 20:51:00 +0000

    Rhythm Game 2

---
+++ Rhythm Gme/Assets/scripts/Note_Object.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note_Object : MonoBehaviour
+{
+    public bool canBePressed;
+    public KeyCode keyToPress;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(keyToPress))
+        {
+            if (canBePressed)
+            {
+                gameObject.SetActive(false);
+            }
+        } 
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canBePressed = true;
+        }
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canBePressed = false;
+        }
+
+
+    }
+
+}
